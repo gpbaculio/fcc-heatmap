b5faab9ac52eae26794fe5eba8ec95705328e7bf
Glendon Philipp <gpbaculio@gmail.com>
2019-05-12 23:08:10 +0800

    query data to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { url } from './constants';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppState {
+  error: string;
+  data: { baseTemperature: number; monthlyVariance: monthlyVariance[] } | null;
+}
+
+interface AppProps {}
+
+interface monthlyVariance {
+  year: number;
+  month: number;
+  variance: number;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = {
+      error: '',
+      data: null
+    };
+  }
+  componentDidMount = async () => {
+    try {
+      const data = await axios
+        .get(url)
+        .then(({ data: { monthlyVariance, baseTemperature } }) => ({
+          baseTemperature,
+          monthlyVariance: monthlyVariance.map((val: monthlyVariance) => ({
+            ...val,
+            month: val.month -= 1
+          }))
+        }));
+      this.setState({ data });
+      console.log('state.data ', this.state.data);
+    } catch (error) {
+      this.setState({ error });
+    }
+  };
+
+  render() {
+    return <div />;
+  }
 }
 
 export default App;
